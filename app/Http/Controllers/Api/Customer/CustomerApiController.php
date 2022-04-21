4f2e892fd5d370e58499e0daa36be5d0492859ec
sithuaung67 <sithu237010@gmail.com>
2022-04-21 15:51:06 +0630

    updae onedevice login

--- app/Http/Controllers/Api/Customer/CustomerApiController.php
+++ app/Http/Controllers/Api/Customer/CustomerApiController.php
@@ -65,10 +65,11 @@ class CustomerApiController extends Controller
 
     public function login_version_one(Request $request)
     {
-        $headers[] = getallheaders();
-        foreach($headers as $value){
-            $device_id=$value['device_id'];
-        }
+        $device_id = \Request::header('device_id');
+        // $headers[] = getallheaders();
+        // foreach($headers as $value){
+        //     $device_id=$value['device_id'];
+        // }
         $customer_phone=$request['customer_phone'];
         $fcm_token=$request['fcm_token'];
         $os_type=(int)$request['os_type'];
@@ -237,10 +238,11 @@ class CustomerApiController extends Controller
     */
     public function update(Request $request)
     {
-        $headers[] = getallheaders();
-        foreach($headers as $value){
-            $device_id=$value['device_id'];
-        }
+        // $headers[] = getallheaders();
+        // foreach($headers as $value){
+        //     $device_id=$value['device_id'];
+        // }
+        $device_id = \Request::header('device_id');
         $id=$request['customer_id'];
         $customer_name=$request['customer_name'];
         $customer_phone=$request['customer_phone'];
