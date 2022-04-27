3cb310d319cccbe785d390e4a41522ef07753296
sithuaung67 <sithu237010@gmail.com>
2022-04-27 14:18:36 +0630

    header

--- app/Http/Controllers/Api/Customer/CustomerApiController.php
+++ app/Http/Controllers/Api/Customer/CustomerApiController.php
@@ -116,11 +116,15 @@ class CustomerApiController extends Controller
 
     public function login_version_two(Request $request)
     {
-        $headers= getallheaders();
-        if(count($headers)==10){
-            $device_id=$headers['device_id'];
-        }else{
-            return response()->json(['success'=>false,'message'=>'you need device_id']);
+        // $headers= getallheaders();
+        // if(count($headers)==10){
+        //     $device_id=$headers['device_id'];
+        // }else{
+        //     return response()->json(['success'=>false,'message'=>'you need device_id']);
+        // }
+        $headers[] = getallheaders();
+        foreach($headers as $value){
+            $device_id=$value['device_id'];
         }
         $customer_phone=$request['customer_phone'];
         $fcm_token=$request['fcm_token'];
@@ -326,16 +330,16 @@ class CustomerApiController extends Controller
     */
     public function update(Request $request)
     {
-        // $headers[] = getallheaders();
-        // foreach($headers as $value){
-        //     $device_id=$value['device_id'];
-        // }
-        $headers= getallheaders();
-        if(count($headers)==10){
-            $device_id=$headers['device_id'];
-        }else{
-            return response()->json(['success'=>false,'message'=>'you need device_id']);
+        $headers[] = getallheaders();
+        foreach($headers as $value){
+            $device_id=$value['device_id'];
         }
+        // $headers= getallheaders();
+        // if(count($headers)==10){
+        //     $device_id=$headers['device_id'];
+        // }else{
+        //     return response()->json(['success'=>false,'message'=>'you need device_id']);
+        // }
         $id=$request['customer_id'];
         $customer_name=$request['customer_name'];
         $customer_phone=$request['customer_phone'];
