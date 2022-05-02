1fe06b521a16a6aeb2a34bb6a3a77d7b201f7f03
sithuaung67 <sithu237010@gmail.com>
2022-05-02 14:16:52 +0630

    login update

--- app/Http/Controllers/Api/Customer/CustomerApiController.php
+++ app/Http/Controllers/Api/Customer/CustomerApiController.php
@@ -75,10 +75,6 @@ class CustomerApiController extends Controller
         }else{
             return response()->json(['success'=>false,'message'=>'you need device_id']);
         }
-        // $headers[] = getallheaders();
-        // foreach($headers as $value){
-        //     $device_id=$value['device_id'];
-        // }
         $customer_id=$request['customer_id'];
         $customer=Customer::where('customer_id',$customer_id)->first();
 
@@ -136,7 +132,7 @@ class CustomerApiController extends Controller
 
         $customer=Customer::where('customer_phone','=',$customer_phone)->first();
 
-        if($customer!=null){
+        if($customer !=null){
 
             if($customer->device_id != $device_id){
                 $title="Another Device Login";
@@ -176,7 +172,11 @@ class CustomerApiController extends Controller
                     "customer_id"=>$customer->customer_id,
                 ]);
             }
-            return response()->json(['success'=>true,'is_old'=>true,'message' => 'this is customer already exit','data'=>$customer]);
+            if($customer->customer_name==null){
+                return response()->json(['success'=>true,'is_old'=>false,'message' => 'this is customer already exit','data'=>$customer]);
+            }else{
+                return response()->json(['success'=>true,'is_old'=>true,'message' => 'this is customer already exit','data'=>$customer]);
+            }
         }else{
             $customers=new Customer();
             $customers->customer_phone=$customer_phone;
@@ -188,6 +188,7 @@ class CustomerApiController extends Controller
             ActiveCustomer::create([
                 "customer_id"=>$customers->customer_id,
             ]);
+
             return response()->json(['success'=>true,'is_old'=>false,'message' => 'this is customer create','data'=>$customers]);
         }
 
@@ -301,27 +302,74 @@ class CustomerApiController extends Controller
                 $result = json_decode($response->getBody());
                 return response()->json(['success'=>true,'message' => 'Success OTP','data'=>$result]);
             }
-
-
-
-
     }
 
     public function otp_check(Request $request)
     {
         $customer_phone= $request['customer_phone'];
-        $otp =  $request['otp'];
+        $otp = $request['otp'];
 
-        $customer=Customer::where('customer_phone','=',$customer_phone)->first();
+        $customer=Customer::where('customer_phone',$customer_phone)->first();
 
-        if($customer!=null){
+        if($customer){
             if ($customer->otp == $otp) {
-                return response()->json(['success'=>true,'message' => 'OTP Check Success']);
+                $headers = $request->header('device_id');
+                if($headers){
+                    $device_id=$headers;
+                }else{
+                    return response()->json(['success'=>false,'message'=>'device id not found']);
+                }
+                $fcm_token=$request['fcm_token'];
+                $os_type=(int)$request['os_type'];
+
+                if($customer->device_id != $device_id){
+                    $title="Another Device Login";
+                    $messages="Your account have been login from another device";
+                    $message = strip_tags($messages);
+                    $path_to_fcm = 'https://fcm.googleapis.com/fcm/send';
+                    $server_key = 'AAAAHUFURUE:APA91bFEvfAjoz58_u5Ns5l-y48QA9SgjICPzChgqVEg_S_l7ftvXrmGQjsE46rzGRRDtvGMnfqCWkksUMu0lDwdfxeTIHZPRMsdzFmEZx_0LIrcJoaUC-CF43XCxbMs2IMEgJNJ9j7E';
+                    $header = array('Authorization:key=' . $server_key, 'Content-Type:application/json');
+                    //Customer
+                    $fcm_token_noti=array();
+                    array_push($fcm_token_noti, $customer->fcm_token);
+
+                    $notification = array('title' => $title, 'body' => $message,'sound'=>'default');
+                    $field=array('registration_ids'=>$fcm_token_noti,'notification'=>$notification,'data'=>['type'=>'another_login','title' => $title,'body' => $message]);
+
+                    $playLoad = json_encode($field);
+                    $curl_session = curl_init();
+                    curl_setopt($curl_session, CURLOPT_URL, $path_to_fcm);
+                    curl_setopt($curl_session, CURLOPT_POST, true);
+                    curl_setopt($curl_session, CURLOPT_HTTPHEADER, $header);
+                    curl_setopt($curl_session, CURLOPT_RETURNTRANSFER, true);
+                    curl_setopt($curl_session, CURLOPT_SSL_VERIFYPEER, false);
+                    curl_setopt($curl_session, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+                    curl_setopt($curl_session, CURLOPT_POSTFIELDS, $playLoad);
+                    $result = curl_exec($curl_session);
+                    curl_close($curl_session);
+                }
+
+                $customer->fcm_token=$fcm_token;
+                $customer->device_id=$device_id;
+                $customer->os_type=$os_type;
+                $customer->update();
+
+                $check=ActiveCustomer::where('customer_id',$customer->customer_id)->whereDate('created_at',date('Y-m-d'))->first();
+                if(empty($check)){
+                    ActiveCustomer::create([
+                        "customer_id"=>$customer->customer_id,
+                    ]);
+                }
+                if($customer->customer_name==null){
+                    return response()->json(['success'=>true,'is_old'=>false,'message' => 'this is customer already exit and OTP Check Success','device_id_log'=>$device_id,'data'=>$customer]);
+                }else{
+                    return response()->json(['success'=>true,'is_old'=>true,'message' => 'this is customer already exit and OTP Check Success','device_id_log'=>$device_id,'data'=>$customer]);
+                }
             } else {
                 return response()->json(['success'=>false,'message' => 'OTP Check Fail']);
             }
         }else{
-            return response()->json(['success'=>false,'message' => 'Customer phone not found']);
+            return response()->json(['success'=>false,'message' => 'the customer phone number not found']);
         }
 
     }
