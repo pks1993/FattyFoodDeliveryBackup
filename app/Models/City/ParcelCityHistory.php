07c97f864cb956c4476b32d4171e8a70d1fdb585
sithuaung67 <sithu237010@gmail.com>
2022-06-10 14:44:22 +0630

    fixed kbz pay today 10Jun2022

--- app/Models/City/ParcelCityHistory.php
+++ app/Models/City/ParcelCityHistory.php
@@ -8,4 +8,9 @@ class ParcelCityHistory extends Model
 {
     protected $guarded=[];
     protected $primaryKey = 'parcel_city_history_id';
+
+    public function parcel_city()
+    {
+        return $this->hasOne('App\Models\City\ParcelCity','parcel_city_id','parcel_city_id');
+    }
 }
