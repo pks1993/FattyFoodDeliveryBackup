7098a1f77cc9d917e863ce0fb898dbde4a7d80cc
sithuaung67 <sithu237010@gmail.com>
2022-07-18 09:57:44 +0630

    update parcel update

---
+++ resources/views/admin/support_center/contact_us.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Fatty Food Delivery</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{asset('plugins/select2/css/select2.min.css')}}">
+    <link rel="stylesheet" href="{{asset('plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css')}}">
+    <script src="{{asset('plugins/select2/js/select2.full.min.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/admin.css')}}">
+    <style>
+        .p-1 {
+        28a745padding: 0.25rem!important;
+        }
+        .rounded {
+        border-radius: 0.25rem!important;
+        }
+        .border-success {
+        border-color: #28a745!important;
+        }
+        .border {
+        border: 1px solid #28a745!important;
+        }
+        .border2 {
+        border-color: #007bff!important;
+        }
+        .border-primary {
+        border: 1px solid #007bff!important;
+        }
+        col-12 {
+        -ms-flex: 0 0 100%;
+        flex: 0 0 100%;
+        max-width: 100%;
+        }
+        .nav>li>a:focus, .nav>li>a:hover {
+            text-decoration: none;
+            background-color: #343a40;
+            color: #fff;
+        }
+        .nav-pills li>#list-tab.active,.nav-pills li>#offered-tab.active,.nav-pills li>#home-tab.active, .nav-pills .show>.nav-link {
+            background-color: #28a745 !important;
+            color: #FFFFFF !important;
+        }
+        .tab-pane{
+            display : none;
+        }
+        .tab-pane.active{
+            display : block;
+        }
+    </style>
+</head>
+<body style="width:100%;font-size:16px !important;">
+    <div style="background-color: black;color:white;padding-top:5px;padding-bottom:5px;text-align:center;font-size:20px;">
+        <strong>Fatty</strong> Food Delivery
+    </div>
+    <div class="container-fluid">
+        <div class="row p-1">
+            <div class="form-group col-12 p-1 text-center">
+                <h1><b>Contact Us</b></h1>
+            </div>
+            <div class="form-group col-12 p-1">
+                <p><span style="margin-left: 2.5em"> If</span> you can’t find the information you need or require further support, our Customer Service agent is here to help.Tell us more about the issue you’re facing so we can help you better.</p>
+                <p><span style="margin-left: 2.5em;">Note : </span>For order related issues, please provide us with the order number and any other relevant details about the issue (payment type, dates, affected items, etc). You may find the order number in the Order Tracking page or in the “Orders & reordering” section in the app. It has a format of xxxx-xxxx. You may also include any supporting attachments to expedite the process.</p>
+                <p>Address: <span style="font-weight:600;">Lashio,Myanmar</span></p>
+                <p>Customer Care: <span style="font-weight:600;"><a href="tel:09456345744">09456345744</a></span></p>
+                <p>Call Center: <span style="font-weight:600;"><a href="tel:09456345744">09456345744</a></span></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
