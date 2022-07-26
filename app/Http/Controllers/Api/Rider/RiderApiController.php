e3d9cf5282ecf2584e876cdc69c6f46750f88e53
sithuaung67 <sithu237010@gmail.com>
2022-07-26 09:39:35 +0630

    update rider home page

--- app/Http/Controllers/Api/Rider/RiderApiController.php
+++ app/Http/Controllers/Api/Rider/RiderApiController.php
@@ -487,7 +487,8 @@ class RiderApicontroller extends Controller
                     * sin(radians(customer_orders.to_drop_latitude))) AS rider_todrop_distance"))
                     ->whereIn("order_id",$noti_order)
                     ->orderBy('created_at','desc')
-                    ->whereIn('order_status_id',['3','11'])
+                    ->whereIn('order_status_id',['3','5','11'])
+                    ->whereNull('rider_id')
                     ->get();
                     $noti_val=[];
                     foreach($noti_rider as $value1){
