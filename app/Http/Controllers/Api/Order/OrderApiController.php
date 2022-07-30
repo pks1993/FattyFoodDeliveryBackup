cb8ed34303c9e3dc6d692a093dd5dc9b645280c5
sithuaung67 <sithu237010@gmail.com>
2022-07-30 09:44:38 +0630

    update paginate 30

--- app/Http/Controllers/Api/Order/OrderApiController.php
+++ app/Http/Controllers/Api/Order/OrderApiController.php
@@ -322,7 +322,6 @@ class OrderApiController extends Controller
         $check_customer=Customer::where('customer_id',$customer_id)->first();
         if(!empty($check_customer)){
             if($order_type=="food"){
-                $active_order_list_count=CustomerOrder::with(['payment_method','order_status','restaurant','rider','foods','foods.sub_item','foods.sub_item.option'])->orderby('created_at','DESC')->where('customer_id',$customer_id)->whereDate('created_at',$date)->whereIn('order_status_id',['1','3','4','5','6','10','19'])->where('order_type','food')->count();
                 $active_order_list=CustomerOrder::with(['payment_method','order_status','restaurant','rider','foods','foods.sub_item','foods.sub_item.option'])->orderby('created_at','DESC')->where('customer_id',$customer_id)->whereDate('created_at',$date)->whereIn('order_status_id',['1','3','4','5','6','10','19'])->where('order_type','food')->paginate(10);
                 if($active_order_list->isNotEmpty()){
                     foreach ($active_order_list as $item)
@@ -333,7 +332,6 @@ class OrderApiController extends Controller
                     $active_order=[];
                 }
 
-                $past_order_list_count=CustomerOrder::with(['payment_method','order_status','restaurant','rider','foods','foods.sub_item','foods.sub_item.option'])->orderby('created_at','DESC')->where('customer_id',$customer_id)->whereDate('created_at',$date)->whereIn('order_status_id',['7','2','8','9','18','20'])->where('order_type','food')->count();
                 $past_order_list=CustomerOrder::with(['payment_method','order_status','restaurant','rider','foods','foods.sub_item','foods.sub_item.option'])->orderby('created_at','DESC')->where('customer_id',$customer_id)->whereDate('created_at',$date)->whereIn('order_status_id',['7','2','8','9','18','20'])->where('order_type','food')->paginate(10);
                 if($past_order_list->isNotEmpty()){
                     foreach ($past_order_list as $value)
@@ -343,7 +341,7 @@ class OrderApiController extends Controller
                 }else{
                     $past_order=[];
                 }
-                // $all_data=CustomerOrder::with(['payment_method','order_status','restaurant','rider','foods','foods.sub_item','foods.sub_item.option'])->orderby('created_at','DESC')->where('customer_id',$customer_id)->whereDate('created_at',$date)->whereIn('order_status_id',['1','3','4','5','6','10','19','7','2','8','9','18','20'])->where('order_type','food')->paginate(10);
+                $all_count=CustomerOrder::with(['payment_method','order_status','restaurant','rider','foods','foods.sub_item','foods.sub_item.option'])->orderby('created_at','DESC')->where('customer_id',$customer_id)->whereDate('created_at',$date)->whereIn('order_status_id',['1','3','4','5','6','10','19','7','2','8','9','18','20'])->where('order_type','food')->count();
                 $active_count=$active_order_list->count();
                 if($active_count==0){
                     $all_data=Paginator::merge($active_order_list,$past_order_list)->sortByDesc('created_at')->get();
@@ -353,7 +351,7 @@ class OrderApiController extends Controller
                     // dd(count($all_data));
                 }
 
-                return response()->json(['success'=>true,'trace'=>['active'=>$active_order_list_count,'past'=>$past_order_list_count],'message'=>"this is customer's of food order",'active_order'=>$active_order,'past_order'=>$past_order,'current_page'=>$all_data->toArray()['current_page'],'first_page_url'=>$all_data->toArray()['first_page_url'],'from'=>$all_data->toArray()['from'],'last_page'=>$all_data->toArray()['last_page'],'last_page_url'=>$all_data->toArray()['last_page_url'],'next_page_url'=>$all_data->toArray()['next_page_url'],'path'=>$all_data->toArray()['path'],'per_page'=>$all_data->toArray()['per_page'],'prev_page_url'=>$all_data->toArray()['prev_page_url'],'to'=>$all_data->toArray()['to'],'total'=>$all_data->toArray()['total']]);
+                return response()->json(['success'=>true,'message'=>"this is customer's of food order",'active_order'=>$active_order,'past_order'=>$past_order,'current_page'=>$all_data->toArray()['current_page'],'first_page_url'=>$all_data->toArray()['first_page_url'],'from'=>$all_data->toArray()['from'],'last_page'=>$all_data->toArray()['last_page'],'last_page_url'=>$all_data->toArray()['last_page_url'],'next_page_url'=>$all_data->toArray()['next_page_url'],'path'=>$all_data->toArray()['path'],'per_page'=>$all_data->toArray()['per_page'],'prev_page_url'=>$all_data->toArray()['prev_page_url'],'to'=>$all_data->toArray()['to'],'total'=>$all_count]);
             }elseif($order_type=="parcel"){
                 $active_order_list=CustomerOrder::with(['customer','parcel_type','parcel_extra','parcel_images','payment_method','order_status','restaurant','rider','customer_address','foods','foods.sub_item','foods.sub_item.option'])->orderby('created_at','DESC')->where('customer_id',$customer_id)->whereDate('created_at',$date)->whereIn('order_status_id',['11','12','13','14','17'])->where('order_type','parcel')->paginate(10);
                 $data=[];
@@ -412,8 +410,18 @@ class OrderApiController extends Controller
                     $past_order=[];
                 }
 
-                $all_data=Paginator::merge($active_order_list,$past_order_list)->sortByDesc('created_at')->get();
-                return response()->json(['success'=>true,'message'=>"this is customer's of parcel order",'active_order'=>$active_order ,'past_order'=>$past_order,'current_page'=>$all_data->toArray()['current_page'],'first_page_url'=>$all_data->toArray()['first_page_url'],'from'=>$all_data->toArray()['from'],'last_page'=>$all_data->toArray()['last_page'],'last_page_url'=>$all_data->toArray()['last_page_url'],'next_page_url'=>$all_data->toArray()['next_page_url'],'path'=>$all_data->toArray()['path'],'per_page'=>$all_data->toArray()['per_page'],'prev_page_url'=>$all_data->toArray()['prev_page_url'],'to'=>$all_data->toArray()['to'],'total'=>$all_data->toArray()['total']]);
+                $all_count=CustomerOrder::with(['payment_method','order_status','restaurant','rider','foods','foods.sub_item','foods.sub_item.option'])->orderby('created_at','DESC')->where('customer_id',$customer_id)->whereDate('created_at',$date)->whereIn('order_status_id',['1','3','4','5','6','10','19','7','2','8','9','18','20'])->where('order_type','parcel')->count();
+                $active_count=$active_order_list->count();
+                if($active_count==0){
+                    $all_data=Paginator::merge($active_order_list,$past_order_list)->sortByDesc('created_at')->get();
+                    // dd(count($all_data));
+                }else{
+                    $all_data=Paginator::merge($past_order_list,$active_order_list)->sortByDesc('created_at')->get();
+                    // dd(count($all_data));
+                }
+
+                // $all_data=Paginator::merge($active_order_list,$past_order_list)->sortByDesc('created_at')->get();
+                return response()->json(['success'=>true,'message'=>"this is customer's of parcel order",'active_order'=>$active_order ,'past_order'=>$past_order,'current_page'=>$all_data->toArray()['current_page'],'first_page_url'=>$all_data->toArray()['first_page_url'],'from'=>$all_data->toArray()['from'],'last_page'=>$all_data->toArray()['last_page'],'last_page_url'=>$all_data->toArray()['last_page_url'],'next_page_url'=>$all_data->toArray()['next_page_url'],'path'=>$all_data->toArray()['path'],'per_page'=>$all_data->toArray()['per_page'],'prev_page_url'=>$all_data->toArray()['prev_page_url'],'to'=>$all_data->toArray()['to'],'total'=>$all_count,'total_paginate'=>$all_data->toArray()['total']]);
             }else{
                 return response()->json(['success'=>false,'message'=>'order_type not found!']);
             }
