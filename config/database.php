cbb06ca739c3fbea3c7746a6fbc71c5475cf62d9
sithuaung67 <sith237010@gmial.com>
2022-10-07 02:29:47 +0630

    database update

--- config/database.php
+++ config/database.php
@@ -55,6 +55,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
+	    'timezone'  => '+06:30',
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
