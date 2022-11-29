b5c795b69dd464e4e2ad5e71fed38b14fdc68717
sithuaung67 <sithu237010@gmail.com>
2022-11-29 11:01:02 +0630

    kpay refund

--- resources/views/admin/src/example/refund.php
+++ resources/views/admin/src/example/refund.php
@@ -65,7 +65,7 @@ try {
             $customer_id=$customer_orders->customer_id;
             $restaurant_id=$customer_orders->restaurant_id;
             $customer_order_id=$customer_orders->customer_order_id;
-            $sql1="INSERT INTO notification_templates (notification_type,order_id,customer_id,restaurant_id,customer_order_id,cancel_amount,noti_type) VALUES ($notification_menu_id,$order_id,$customer_id,$restaurant_id,$customer_order_id,$refund_amount,$noti_type)";
+            $sql1="INSERT INTO notification_templates (notification_type,order_id,customer_id,restaurant_id,customer_order_id,cancel_amount,noti_type) VALUES ($notification_menu_id,$order_id,$customer_id,$restaurant_id,$customer_order_id,$refund_amount,'$noti_type')";
 
             $sql="INSERT INTO order_kbz_refunds (order_id,result,code,msg,merch_order_id,merch_code,trans_order_id,refund_status,refund_order_id,refund_amount,refund_currency,refund_time,nonce_str,sign_type,sign) VALUES ($order_id,$result1,$code,$msg,$merch_order_id,$merch_code,$trans_order_id,$refund_status,$refund_order_id,$refund_amount,$refund_currency,$refund_time,$nonce_str,$sign_type,$sign)";
 
