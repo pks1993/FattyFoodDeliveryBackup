9a524b59aa1905668f50637eeff4e42c14e92dd1
sithuaung67 <sithu237010@gmail.com>
2022-11-30 19:38:02 +0630

    update api kpay refund

--- resources/views/admin/src/example/each_refund.php
+++ resources/views/admin/src/example/each_refund.php
@@ -69,13 +69,13 @@ try {
             $customer_order_id=$customer_orders->customer_order_id;
 
             // $amount=(int)$refund_amount;
-            $payment_total=$payment_total_amount-(int)$refund_amount;
+            $payment_total=$payment_total_amount-$refundAmount;
 
             $sql1="INSERT INTO notification_templates (notification_type,order_id,customer_id,restaurant_id,customer_order_id,cancel_amount,noti_type,created_at,updated_at) VALUES ($notification_menu_id,$order_id,$customer_id,$restaurant_id,$customer_order_id,$refund_amount,'$noti_type',now(),now())";
 
             $is_partial_refund=1;
             $sql="INSERT INTO order_kbz_refunds (order_id,is_partial_refund,result,code,msg,merch_order_id,merch_code,trans_order_id,refund_status,refund_order_id,refund_amount,refund_currency,refund_time,nonce_str,sign_type,sign,created_at,updated_at) VALUES ($order_id,$is_partial_refund,$result1,$code,$msg,$merch_order_id,$merch_code,$trans_order_id,$refund_status,$refund_order_id,$refund_amount,$refund_currency,$refund_time,$nonce_str,$sign_type,$sign,now(),now())";
-	        $orde_update = "UPDATE customer_orders SET is_partial_refund=1,restaurant_remark = '$payment_total' WHERE order_id=$orderId;";
+	        $orde_update = "UPDATE customer_orders SET is_partial_refund=1,	payment_total_amount = '$payment_total' WHERE order_id=$orderId;";
 
             if ($conn->query($sql) === TRUE && $conn->query($orde_update) === TRUE && $conn->query($sql1) === TRUE) {
                 $arrayName = array('success' =>true,'message'=>"successfully cancel food order by customer",'merchOrderId_log'=>$merchOrderId,'data'=>['response'=>$response,'order'=>$customer_orders]);
