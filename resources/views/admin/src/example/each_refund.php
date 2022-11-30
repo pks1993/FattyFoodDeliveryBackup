e7985e5ee25f1a038eeba08209d2d5c62929aa6d
sithuaung67 <sithu237010@gmail.com>
2022-11-30 19:26:36 +0630

    update api kpay refund

--- resources/views/admin/src/example/each_refund.php
+++ resources/views/admin/src/example/each_refund.php
@@ -68,8 +68,8 @@ try {
             $restaurant_id=$customer_orders->restaurant_id;
             $customer_order_id=$customer_orders->customer_order_id;
 
-            $amount=(int)$refund_amount;
-            $payment_total=$payment_total_amount-$amount;
+            // $amount=(int)$refund_amount;
+            $payment_total=$refund_amount."/".$payment_total_amount;
 
             $sql1="INSERT INTO notification_templates (notification_type,order_id,customer_id,restaurant_id,customer_order_id,cancel_amount,noti_type,created_at,updated_at) VALUES ($notification_menu_id,$order_id,$customer_id,$restaurant_id,$customer_order_id,$refund_amount,'$noti_type',now(),now())";
 
